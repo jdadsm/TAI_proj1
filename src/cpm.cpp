99bb46570679fc0676d5942d2a43c7b041a843a4
jmourao <joaomourao2002@gmail.com>
2024-02-17 19:18:31 +0000

    Added project base and generator to read files

---
+++ src/cpm.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+class Generator {
+private:
+    std::ifstream file;
+    std::string buffer;
+    long position;
+    int chunkSize;
+
+public:
+    Generator(const std::string& filename, int chunkSize = 4) : file(filename), chunkSize(chunkSize) {
+        position = 0;
+        if (!file.is_open()) {
+            std::cerr << "Failed to open file: " << filename << std::endl;
+        }
+    }
+
+    ~Generator() {
+        file.close();
+    }
+
+    std::string getNextChunk() {
+        if (!file.is_open()) {
+            std::cerr << "File is not open!" << std::endl;
+            return "";
+        }
+
+        buffer.clear();
+        char c;
+        int count = 0;
+        while (count < chunkSize && file.get(c)) {
+            if (c == 'A' || c == 'C' || c == 'G' || c == 'T') {
+                buffer.push_back(c);
+                count++;
+            }
+        }
+
+        file.seekg(position + 1);
+        position = file.tellg();
+
+        return buffer;
+    }
+
+    bool hasMoreData() const {
+        return position != -1;
+    }
+};
+
+
+int main() {
+
+    const std::string filename = "../../example/example.txt";
+    Generator generator(filename);
+
+    while(generator.hasMoreData()){
+        std::string dnaChunk = generator.getNextChunk();
+        std::cout << dnaChunk << std::endl;
+    }
+    
+    return 0;
+}
