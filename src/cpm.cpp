fd846631cf89e55120b73314e1df16e9bcd44ed0
jmourao <joaomourao2002@gmail.com>
2024-02-23 18:08:56 +0000

    added CopyModel class base

--- src/cpm.cpp
+++ src/cpm.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <unordered_map>
+#include <cstdio>
+#include <vector>
+
 
 class Generator {
 private:
@@ -10,8 +14,8 @@ private:
     int chunkSize;
 
 public:
-    Generator(const std::string& filename, int chunkSize = 4) : file(filename), chunkSize(chunkSize) {
-        position = 0;
+    Generator(std::string& filename, int chunkSize) : file(filename), chunkSize(chunkSize) {
+        position = chunkSize-1;
         if (!file.is_open()) {
             std::cerr << "Failed to open file: " << filename << std::endl;
         }
@@ -30,32 +34,88 @@ public:
         buffer.clear();
         char c;
         int count = 0;
+        file.seekg(position - chunkSize + 1);
         while (count < chunkSize && file.get(c)) {
             buffer.push_back(c);
             count++;
         }
 
-        file.seekg(position + 1);
-        position = file.tellg();
+        file.seekg(position+2);
+        if(file.fail()){
+            position = -1;
+        }else{
+            position += 1;
+        }
+
+        printf("\nbuffer:%s\tposition:%ld",buffer.c_str(),position);
 
         return buffer;
     }
 
-    bool hasMoreData() const {
-        return position != -1;
+    bool hasMoreData() {
+        return position!=-1;
+    }
+    
+    long getPosition() {
+        return position;
     }
 };
 
+class CopyModel{
+private:
+    std::string method;
+    std::unordered_map<std::string, int> hashTable;
+    Generator generator;
 
-int main() {
+public:
+    
+    CopyModel(std::string& filename, int chunkSize,std::string method="last"): generator(filename,chunkSize), method(method){
+        if(method == "last"){
+            printf("\nlast method");
+        }
+    }
 
-    const std::string filename = "../../example/tiny_example.txt";
-    Generator generator(filename);
-
-    while(generator.hasMoreData()){
-        std::string dnaChunk = generator.getNextChunk();
-        std::cout << dnaChunk << std::endl;
+    void fillHash(){
+        printf("\nfillHash");
+        printf("\nmethod:%s",method.c_str());
+        if (method == "last"){
+            printf("here");
+            while(generator.hasMoreData()){
+                std::string chunk = generator.getNextChunk();
+                int position = generator.getPosition();
+                hashTable[chunk] = position;
+            }
+        }
+        
+        for (auto it = hashTable.begin(); it != hashTable.end(); ++it) {
+            printf("\n%s:%d",it->first.c_str(),it->second);
+        }
     }
     
+    double prob_hit(int hits, int misses, int alpha = 2){
+        return (hits+alpha)/(hits+misses+2*alpha);
+    }
+
+};
+
+
+int main(int argc, char* argv[]) {
+    std::string filename = "";
+    int chunkSize = 0;
+    if(argc == 2){
+        filename = argv[1];
+        chunkSize = 4;
+    } else if(argc == 3){
+        filename = argv[1];
+        chunkSize = std::stoi(argv[2]);
+    } else {
+        printf("Usage: %s <filename> [chunkSize]\n", argv[0]);
+        exit(1);
+    }
+    printf("\nfilename:%s",filename.c_str());
+    printf("\nchunkSize:%d",chunkSize);
+    CopyModel copymodel = CopyModel(filename,chunkSize);
+    copymodel.fillHash();
+    printf("\n");
     return 0;
 }
