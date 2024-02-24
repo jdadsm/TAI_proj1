506d346bead081087149f9bf41f075f006e4f7f3
jmourao <joaomourao2002@gmail.com>
2024-02-24 17:30:09 +0000

    Added hits and misses count and fixed some generator issues

--- src/cpm.cpp
+++ src/cpm.cpp
@@ -4,18 +4,21 @@
 #include <unordered_map>
 #include <cstdio>
 #include <vector>
+#include <cmath>
 
-
+// class to prevent loading the whole file into memory
 class Generator {
 private:
     std::ifstream file;
     std::string buffer;
     long position;
     int chunkSize;
+    bool reachedEOF;
 
 public:
     Generator(std::string& filename, int chunkSize) : file(filename), chunkSize(chunkSize) {
         position = chunkSize-1;
+        reachedEOF = false;
         if (!file.is_open()) {
             std::cerr << "Failed to open file: " << filename << std::endl;
         }
@@ -31,10 +34,6 @@ public:
             return "";
         }
 
-        if(position == -1){
-            return NULL;
-        }
-
         buffer.clear();
         char c;
         int count = 0;
@@ -43,24 +42,45 @@ public:
             buffer.push_back(c);
             count++;
         }
+        
+        // this line is needed to update the failbit that is triggered when EOF is reached
+        file.get(c); 
 
-        position=file.tellg();
+        if(file.tellg() == -1){
+            reachedEOF = true;
+        }
+        position+=1;        
+        
         //printf("\nbuffer:%s\tposition:%ld",buffer.c_str(),position);
 
         return buffer;
     }
 
     bool hasMoreData() {
-        return position!=-1;
+        return !reachedEOF;
     }
     
     long getPosition() {
         return position;
     }
+
+    // returns '\0' if the position does not contain a character
+    char getChar(long pos){
+        char c;
+        file.seekg(pos);
+        file.get(c);
+        if(c == std::ifstream::traits_type::eof()){
+            c = '\0';
+        }
+        return c;
+    }
 };
 
 class CopyModel{
 private:
+    long hits;
+    long misses;
+    long tries;
     std::string method;
     std::unordered_map<std::string, int> hashTable;
     Generator generator;
@@ -68,32 +88,56 @@ private:
 public:
     
     CopyModel(std::string& filename, int chunkSize,std::string method="last"): generator(filename,chunkSize), method(method){
+        hits = 0;
+        misses = 0;
+        tries = 0;
         if(method == "last"){
             //printf("\nlast method");
         }
     }
 
     void fillHash(){
-        //printf("\nfillHash");
         //printf("\nmethod:%s",method.c_str());
         if (method == "last"){
-            while(true){
+            while(generator.hasMoreData()){
                 std::string chunk = generator.getNextChunk();
                 int position = generator.getPosition();
-                if(!generator.hasMoreData()){
-                    break;
+                
+                printf("\n%s-%c:%d",chunk.c_str(),generator.getChar(position),position);
+
+                if(hashTable.count(chunk) != 0){ // if it is not the first time the chunk has appeared
+                    if(predictionIsCorrect(hashTable[chunk],position)){
+                        hits+=1;
+                    }else{
+                        misses+=1;
+                    }
                 }
+                tries+=1;
+
                 hashTable[chunk] = position;
             }
         }
         
         for (auto it = hashTable.begin(); it != hashTable.end(); ++it) {
-            printf("\n%s:%d",it->first.c_str(),it->second);
+            //printf("\n%s:%d",it->first.c_str(),it->second);
         }
+
+        printf("\ntries:%ld\nhits:%ld\nmisses:%ld",tries,hits,misses);
+    }
+
+    bool predictionIsCorrect(long lastPosition, long predictionPosition){
+        char lastChar = generator.getChar(lastPosition);
+        char predictionChar = generator.getChar(predictionPosition);
+        return lastChar == predictionChar;
     }
     
     double prob_hit(int hits, int misses, int alpha = 2){
         return (hits+alpha)/(hits+misses+2*alpha);
+    }
+
+    //returns estimated amount of information for a symbol
+    double eai(double probability){
+        return -log2(probability);
     }
 
 };
